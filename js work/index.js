883d2e164a26deee7a291ecb026cc783e3cb950b
Divya <divyajain3419@gmail.com>
2024-07-30 01:21:59 +0530

    practice

---
+++ js work/index.js
@@ -0,0 +1,22 @@
+console.log(typeof null)// object
+console.log(1=="1")// true
+
+var arr=[1,2,3]
+arr[0]="p"
+console.log(arr)
+
+var str="masai"
+str="ram"
+console.log(str)
+
+var str="masai"
+str="ram"
+console.log(str)
+
+let admin={
+    name:"rita"
+}
+Object.freeze(admin)
+admin.name="rahul"
+
+console.log(admin)
