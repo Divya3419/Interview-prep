d7136c02c2980efce30cf64b899ff06a3a31bbaf
Divya <divyajain3419@gmail.com>
2024-11-12 23:11:15 +0530

    topics cover

--- foreachormap/index.js
+++ foreachormap/index.js
@@ -1,6 +1,40 @@
-let arr=[1,2,3]
+// let arr=[1,2,3]
 
-arr.forEach((element)=>{
-    console.log(element)
-})
+// arr.forEach((element)=>{
+//     console.log(element)
+// })
+
+// let arr1=[1,2,3]
+
+// let res=arr1.map((element)=>{
+//     return element
+// })
+
+// console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+console.log(5==5)
+console.log(5==="5")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
