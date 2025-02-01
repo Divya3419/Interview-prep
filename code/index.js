b8502a94c4dd63eda36b2a566e943f87e97158c7
Divya <divyajain3419@gmail.com>
2025-02-02 03:27:11 +0530

    add code

---
+++ code/index.js
@@ -0,0 +1,30 @@
+// let a=NaN
+// let b=NaN
+// console.log(a!=b)
+// console.log(a!==b)
+
+
+
+
+// let a="20";
+
+// console.log(a+1)
+// console.log(++a)
+// console.log(a++)
+
+
+
+
+
+// let n = 1
+// let sum = ++n + n++
+// console.log(sum)
+
+
+
+
+
+
+console.log("2"+1)
+console.log("2"-1)
+
